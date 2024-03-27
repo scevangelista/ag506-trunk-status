efa2b55d01c992a427c3c4e0d4f6e8884b5d8037
Steve E <44708676+scevangelista@users.noreply.github.com>
2024-03-27 20:55:49 -0300

    Add Flask, Logging

---
+++ app/app.py
@@ -0,0 +1,23 @@
+import json
+import logging
+from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Log
+logging.basicConfig(filename="./log/worker.log", level=logging.INFO)
+
+# Returns a message that is working
+@app.route('/', methods=['GET'])
+def info():
+    print( json.dumps( {"status": True, "message": "Working..."} ) )
+    logging.info( datetime.now().strftime('%Y-%m-%d_%H:%M:%S') +' - '+ json.dumps( {"status": True, "message": "Working..."} ) )
+    return json.dumps( {"status": True, "message": "Working..."} )
+
+# Customizes 404
+@app.errorhandler(404)
+def page_not_found(e):
+    print( json.dumps( {"status": False, "message": "Resource not found"} ) )
+    logging.info( datetime.now().strftime('%Y-%m-%d_%H:%M:%S') +' - '+ json.dumps( {"status": False, "message": "Resource not found"} ) )
+    return json.dumps( {"status": False, "message": "Resource not found"} ), 404
