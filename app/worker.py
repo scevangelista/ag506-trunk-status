fdccc615e6915ac5dcebafbfb8fbb4d21b943087
Steve E <44708676+scevangelista@users.noreply.github.com>
2024-03-27 21:58:46 -0300

    .

--- app/worker.py
+++ app/worker.py
@@ -7,62 +7,62 @@ from urllib.parse import urlparse
 
 
 # Checks received parameters and minimally validates
-def validateParams( url, user, password ):
-    url = html.escape( url )
-    user = html.escape( user )
+def validateParams(url, user, password):
+    url = html.escape(url)
+    user = html.escape(user)
 
-    if( len( user ) < 5 ):
-        return { "sucess": False, "message": "The user must have more than 4 characters" }
+    if len( user ) < 5:
+        return {"sucess": False, "message": "The user must have more than 4 characters"}
 
     if( len( password ) < 5 ):
-        return { "sucess": False, "message": "The password must have more than 4 characters" }
+        return {"sucess": False, "message": "The password must have more than 4 characters"}
 
-    oUrl = urlparse( url )
-    if( not( oUrl.geturl() ) ):
-        return { "sucess": False, "message": "The url is invalid" }
+    oUrl = urlparse(url)
+    if(not(oUrl.geturl())):
+        return {"sucess": False, "message": "The url is invalid"}
 
-    return { "sucess": True, "url": oUrl.geturl(), "user": user, "password": password }
+    return {"sucess": True, "url": oUrl.geturl(), "user": user, "password": password}
 
 
 # Query the equipment and return normalized data
-def getData( url, user, password ):
+def getData(url, user, password):
 
-    resVal = validateParams( url, user, password )
-    if not( resVal["sucess"] ):
-        return json.dumps( resVal )
+    resVal = validateParams(url, user, password)
+    if not(resVal["sucess"]):
+        return json.dumps(resVal)
 
     # Authenticate to the device and save the session for future requests
     session = requests.Session()
-    resAuth = session.post( resVal["url"] + "/app/do.login", { "username": user, "password": password } )
+    resAuth = session.post(resVal["url"]+"/app/do.login", {"username": user, "password": password})
 
     if resAuth.status_code == 200:
 
         # Get page with trunk status
-        resData = session.get( resVal["url"] + "/app/do.status" )
+        resData = session.get(resVal["url"]+"/app/do.status")
 
         if resData.status_code == 200:
 
             # Read page and create response
-            page = BeautifulSoup( resData.text, "html.parser" )
-            trunks = page.findAll( "div", attrs={ "class": "e1_status" } )
+            page = BeautifulSoup(resData.text, "html.parser")
+            trunks = page.findAll("div", attrs={"class": "e1_status"})
 
             res = {}
-            res[ "status" ] = True
+            res["status"] = True
 
             for trunk in trunks:
 
                 data = {}
-                data["name"]  = trunk.find( "span", attrs={ "class": "hr_text" } ).text
-                data["alarm"] = trunk.find( "table", attrs={ "class": "e1_alarms" } ).find( "td" ).text
+                data["name"] = trunk.find("span", attrs={"class": "hr_text"}).text
+                data["alarm"] = trunk.find("table", attrs={"class": "e1_alarms"}).find("td").text
 
-                for status in trunk.find( "table", attrs={ "class": "e1_stats" } ).findAll( "tr" ):
+                for status in trunk.find("table", attrs={"class": "e1_stats"}).findAll("tr"):
 
-                    details = status.findAll( "td" )
+                    details = status.findAll("td")
 
-                    data[ details[0].text.replace(" ", "_").lower() ] = details[1].text
-                    data[ details[0].text.replace(" ", "_").lower() + "_total" ] = details[2].text
+                    data[details[0].text.replace(" ", "_").lower()] = details[1].text
+                    data[details[0].text.replace(" ", "_").lower()+"_total"] = details[2].text
 
-                res[ data[ "name" ] ] = data
+                res[data["name"]] = data
 
             return res
 
